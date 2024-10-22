7d0ade0ddd5d5d23ed6e6cc3de5d6d0578cae0a1
geoson <balsohn@naver.com>
2024-10-22 16:08:09 +0900

    react

--- src/main/java/kr/co/react/config/WebConfig.java
+++ src/main/java/kr/co/react/config/WebConfig.java
@@ -1,15 +1,28 @@
 package kr.co.react.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	// 배포용 WebConfig
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")                
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(false);
+    }
+    
+    
+    // 개발용 WebConfig
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")  // React 서버
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
